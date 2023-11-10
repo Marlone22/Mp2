a1abefe31ea148d877cc0bcd15ccb0f58da65b6c
Marlone22 <marlonemateo2324@gmail.com>
2023-11-10 12:11:37 +0800

    commit

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,57 +1,31 @@
-import { useState, useEffect } from 'react';
+import { useState } from "react";
 
-function SearchExercises() {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [exercises, setExercises] = useState([]);
+export default function SearchExercises({ onSearch }) {
+  const [inputValue, setInputValue] = useState("");
 
-  useEffect(() => {
-    fetch('https://exercisedb.p.rapidapi.com/exercises')
-      .then(response => response.json())
-      .then(data => setExercises(data))
-      .catch(error => console.error('Error fetching exercises:', error));
-  }, []);
-
-  const filterExercises = () => {
-    const lowercaseQuery = searchQuery.toLowerCase();
-
-
-    const filteredExercises = exercises.filter(exercise =>
-      exercise.toLowerCase().includes(lowercaseQuery)
-    );
-
-    return filteredExercises;
-  };
-
-  const handleSearch = () => {
-    const filteredExercises = filterExercises();
-    console.log('Filtered Exercises:', filteredExercises);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearch(inputValue);
   };
 
   return (
-    <>
-      <div className="flex flex-col items-center mt-10 justify-center p-5">
-        <h2 className="font-bold text-3xl lg:text-6xl mb-5 text-center">
-          Awesome Exercises You Should Know
-        </h2>
-
-        <div className="relative mb-12">
-          <input
-            className="w-80 lg:w-96 h-16 pl-5 pr-16 bg-white rounded-full border-none font-semibold"
-            type="text"
-            placeholder="Search Exercises"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
-          <button
-            className="search-btn bg-red-600 text-white  rounded-full px-6 h-16 absolute top-0 right-0"
-            onClick={handleSearch}
-          >
-            Search
-          </button>
-        </div>
-      </div>
-    </>
+    <form onSubmit={handleSubmit} className="my-4 mx-auto max-w-md">
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        className="w-full p-2 border border-gray-300 rounded-md"
+        placeholder="Enter body part"
+      />
+      <button type="submit" className="block mx-auto mt-2 bg-blue-500 text-white py-2 px-4 rounded-md">
+        Search
+      </button>
+    </form>
   );
 }
 
-export default SearchExercises;
+
+
+
+
+
--- src/pages/Exercises.jsx
+++ src/pages/Exercises.jsx
@@ -1,15 +1,29 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import SearchExercises from "../components/SearchExercises";
 
 export default function Exercises() {
+  const [bodyPart, setBodyPart] = useState(""); 
   const [data, setData] = useState([]);
-  
 
   useEffect(() => {
-    fetch('https://exercisedb.p.rapidapi.com/exercises')
-      .then((response) => response.json())
-      .then((json) => console.log(json))
-  }, [])
+    if (bodyPart) {
+      fetch(`https://exercisedb.p.rapidapi.com/exercises/bodyPartList`, {
+        headers: {
+          'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+          'X-RapidAPI-Key': '8a4f5a6ca5msh2da4b416502f1bap175610jsn6ff0cd998571' 
+        }
+      })
+      .then(response => response.json())
+      .then(json => {
+        setData(json);
+      })
+      .catch(error => console.error('Error fetching data:', error));
+    }
+  }, [bodyPart]);
+
+  const handleSearch = (bodyPart) => {
+    setBodyPart(bodyPart);
+  };
 
   return (
     <>
@@ -18,10 +32,10 @@ export default function Exercises() {
           <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Exercises</h1>
         </div>
       </div>
-      <SearchExercises />
+      <SearchExercises onSearch={handleSearch} />
 
       <div className="flex flex-wrap justify-center p-4">
-        {data.map((item, i) => (
+        {data.length > 0 && data.map((item, i) => (
           <div key={i} className="max-w-md mx-4 mb-8 bg-white rounded-lg shadow-lg">
             <div className="p-4">
               <h2 className="text-xl font-semibold mb-2">{item.title}</h2>
